27f467a3594a06c812545ccf2a6e6b9cc90c7ee6
kuanchiliao1 <kuanchiliao@gmail.com>
2023-07-17 18:56:01 -0700

    Add example code

---
+++ test.js
@@ -0,0 +1,12 @@
+const someOrder = {
+  items: [
+    {name: 'Dragon food', price: 8},
+    {name: 'Dragon cage (small)', price: 800}
+  ]
+}
+
+const orderTotal = order => order.items
+  .reduce((prev, cur) => prev + cur.price, 0)
+
+result = orderTotal(someOrder)
+result
