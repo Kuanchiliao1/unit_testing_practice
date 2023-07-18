e276fb72e8abacc3ec27ce13212772edc6055b4b
kuanchiliao1 <kuanchiliao@gmail.com>
2023-07-17 20:42:25 -0700

    Convert vanilla tests to Jest compatible tests

--- order-total.test.js
+++ order-total.test.js
@@ -1,36 +1,33 @@
-const orderTotal = require('order-total')
+const orderTotal = require('./order-total')
 
-if (orderTotal({
-  items: [
-    {name: 'Dragon food', price: 8 },
-    {name: 'Dragon cage (small)', price: 800}
-  ]
-}) !== 808) {
-  throw new Error('Check fail: happy path (1)')
-}
+it('Quantity', () => 
+  expect(orderTotal({
+    items: [
+      { name: 'Dragon candy', price: 2, quantity: 3 }
+    ]
+  })).toBe(6))
 
-if (orderTotal({
-  items: [
-    { name: 'Dragon candy', price: 2, quantity: 3 }
-  ]
-}) !== 6) {
-  throw new Error('Check fail: Quantity')
-}
+it('happy path (1)', () => 
+  expect(orderTotal({
+    items: [
+      {name: 'Dragon food', price: 8 },
+      {name: 'Dragon cage (small)', price: 800}
+    ]
+  })).toBe(808))
 
-if (orderTotal({
-  items: [
-    {name: 'Dragon candy', price: 3}
-  ]
-}) !== 3) {
-  throw new Error('Check fail: no quantity specified')
-}
+it('no quantity specified', () => {
+  return expect(orderTotal({
+    items: [
+      {name: 'Dragon candy', price: 3}
+    ]
+  })).toBe(3)
+})
 
-if (orderTotal({
-  items: [
-    {name: 'Dragon collar', price: 20, quantity: 1 },
-    {name: 'Dragon chew toy', price: 40, quantity: 1}
-  ]
-}) !== 60) {
-  throw new Error('Check fail: happy path (2)')
-}
-
+it('happy path (2)', () => {
+  return expect(orderTotal({
+    items: [
+      {name: 'Dragon collar', price: 20, quantity: 1 },
+      {name: 'Dragon chew toy', price: 40, quantity: 1}
+    ]
+  })).toBe(60)
+})
