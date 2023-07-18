126d0b816cfe6f6e9913cf21ca750878d8f64b94
kuanchiliao1 <kuanchiliao@gmail.com>
2023-07-17 19:15:12 -0700

    Add test and update orderTotal()

--- test.js
+++ test.js
@@ -4,5 +4,20 @@ if (orderTotal({
     {name: 'Dragon cage (small)', price: 800}
   ]
 }) !== 808) {
-  throw new Error('Check fail: happy path')
+  throw new Error('Check fail: happy path (1)')
+}
+
+if (orderTotal({
+  items: [
+    {name: 'Dragon collar', price: 20 },
+    {name: 'Dragon chew toy', price: 40}
+  ]
+}) !== 60) {
+  throw new Error('Check fail: happy path (2)')
+}
+
+function orderTotal(order) {
+  return order.items.reduce((total, item) => {    
+    return total + item.price
+  }, 0)
 }
